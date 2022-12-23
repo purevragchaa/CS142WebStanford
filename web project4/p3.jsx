62a8e8696ef3bdde5dc4abf518e8a1480a4c688a
purevragchaa <107909304+purevragchaa@users.noreply.github.com>
2022-12-23 18:17:21 +0800

    Delete web project4 directory

--- web project4/p3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './components/Header/header';
-
-
-ReactDOM.render(
-  <Header/>,
-  document.getElementById('reactapp'),
-);
